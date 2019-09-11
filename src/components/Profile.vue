38732649bd152f6df20ab682c77b96d4c4b75be4
Ally Weatherall <aweath01@gmail.com>
2019-09-11 19:17:01 -0400

    Added updated files

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -1,31 +1,33 @@
 <template>
 <div>
    <div id='profile-wrapper'>
-      <div class='profile'>
+      <div class='profile photo'>
          <img src='../assets/profile-pic.jpg' id="picture">
          <div class="font-change">
             <h2 class="center">Alexandra Weatherall</h2>
             <h4 class="center">Junior Web Developer</h4>   
          </div>
       </div>
-      <div class='profile font-change'>
+      <div class='profile font-change experience'>
          <p>
             <h4>Education & Experience</h4>
-            <a href='https://www.senecacollege.ca/programs/fulltime/CPD.html' target='_blank'><strong>Seneca College - Computer Programming (2019) </strong><i class="fas fa-external-link-alt fa-xs"></i></a>
+            <span><a href='https://www.senecacollege.ca/programs/fulltime/CPD.html' target='_blank'><strong>Seneca College - Computer Programming (2019) </strong><i class="fas fa-external-link-alt fa-xs"></i></a></span>
             <br/>
-            Studied C++, JavaScript, Java, SQL, HTML, CSS, Typescript, RPGLE, CLLE
-            <br/> <br/>
+            <ul>
+               <li>C++, JavaScript, Java, SQL, HTML, CSS, Typescript, RPGLE, CLLE</li>
+               <li>Back-End Development using Node.js</li>
+            </ul>
             Front-End Development
             <ul>
                <li><routerLink to='/vue'><strong>Vue.js</strong></routerLink></li>
                <li><routerLink to='/react'><strong>React.js</strong></routerLink></li>
                <li><a href="https://fathomless-thicket-65337.herokuapp.com" target="_blank"><strong>Angular <i class="fas fa-external-link-alt fa-xs"></i></strong></a></li>
             </ul>
-            Back-End Development using Node.js
          </p>
          <div id='linx'>
-            <span> <strong> <a href='mailto:aweath01@gmail.com' target="_blank">Contact Me</a> </strong> </span>
-            <span> <strong> <a href='https://www.github.com/aweath01/profile-site' target="_blank">Source Code</a> </strong> </span>
+            <span> <strong> <a href="https://www.linkedin.com/in/alexandra-weatherall/" target="_blank"><img src='../assets/LI-In-Bug.png' class='logo'></a> </strong> </span>
+            <span> <strong> <a href='https://www.github.com/aweath01/profile-site' target="_blank"><img src='../assets/GitHub-Mark-32px.png'></a> </strong> </span>
+            <span> <strong> <a href='mailto:aweath01@gmail.com' target="_blank"><i class="fas fa-envelope" id="fa-envelope"></i></a> </strong> </span>
          </div>
       </div>
    </div>
@@ -64,8 +66,16 @@ a{
    vertical-align: middle;
    background-color: white;
 }
+.experience{
+   flex: 2;
+}
+.photo{
+   flex: 1;
+}
 #picture{
-   max-width: 100%;
+   width: 283px;
+   height: 378px;
+   align-content: center;
 }
 #linx{
    display: flex;
@@ -83,13 +93,21 @@ span{
 .font-change{
    font-family: 'Lexend Deca', sans-serif;
 }
+.logo{
+   width:31.75px;
+   height:27px;
+   margin-top: 4px;
+}
+#fa-envelope{
+   font-size: 20pt;
+}
 
 @import url('https://fonts.googleapis.com/css?family=Lexend+Deca&display=swap');
 @media screen and (max-width: 600px) {
   #profile-wrapper {
    flex-direction: column;
   }
-  #photo{
+  #picture{
    padding-top: 0%;
    padding-bottom: 0%;
    max-width: 100%;
